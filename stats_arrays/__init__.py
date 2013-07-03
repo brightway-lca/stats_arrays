6cda892decb474df0d823d3bb6d0ed22e2398cdf
Chris Mutel <cmutel@gmail.com>
2013-07-03 19:14:13 +0200

    Add ReST readme

--- stats_arrays/__init__.py
+++ stats_arrays/__init__.py
@@ -1,6 +1,6 @@
 # TODO: Write Monte Carlo tests
 
-__version__ = (0, 1, "alpha1")
+__version__ = (0, 1, "alpha")
 
 from .distributions import *
 from .uncertainty_choices import uncertainty_choices
