9da6de525405fecee8b18588e7ecaf09842c6d86
Chris Mutel <cmutel@gmail.com>
2015-05-11 15:32:33 +0200

    Common base class for errors

--- stats_arrays/errors.py
+++ stats_arrays/errors.py
@@ -1,33 +1,38 @@
-class ImproperBoundsError(StandardError):
+class StatsArraysError(Exception):
+    """Base class for `stats_arrays` errors"""
+    pass
+
+
+class ImproperBoundsError(StatsArraysError):
     """The bounds and mean specified are illegal for this distribution."""
     pass
 
 
-class MaximumIterationsError(StandardError):
+class MaximumIterationsError(StatsArraysError):
     """Drawing random numbers from the distribution to fit in the bounds specified used more than the maximum number of iterations allowed."""
     pass
 
 
-class UnknownUncertaintyType(StandardError):
+class UnknownUncertaintyType(StatsArraysError):
     """The uncertainty type is not defined in uncertainty_choices."""
     pass
 
 
-class UndefinedDistributionError(StandardError):
+class UndefinedDistributionError(StatsArraysError):
     """Values were attempted to be calculated by an undefined distribution."""
     pass
 
 
-class InvalidParamsError(StandardError):
+class InvalidParamsError(StatsArraysError):
     """Invalid params array passed to uncertainty distribution init."""
     pass
 
 
-class MultipleRowParamsArrayError(StandardError):
+class MultipleRowParamsArrayError(StatsArraysError):
     """A function or method which doesn't accept it was passed a params array with more than one row."""
     pass
 
 
-class UnreasonableBoundsError(StandardError):
+class UnreasonableBoundsError(StatsArraysError):
     """The provided bounds cover an unreasonably small section of the distribution sample space."""
     pass
